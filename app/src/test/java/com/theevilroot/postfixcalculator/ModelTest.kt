b62449e228b13b1d1aec5731820bcfe7049fb356
TheEvilRoot <robot-creatix@ya.ru>
2019-06-29 18:30:02 +0300

    Add unit test

---
+++ app/src/test/java/com/theevilroot/postfixcalculator/ModelTest.kt
@@ -0,0 +1,26 @@
+package com.theevilroot.postfixcalculator
+
+import com.theevilroot.postfixcalculator.main.impl.PostfixModelImpl
+import org.junit.Assert.*
+import org.junit.Test
+
+class ModelTest {
+
+    @Test
+    fun testToPostfix() {
+        val model = PostfixModelImpl()
+        val result = model.convertToPostfix("(1 + 3 - (4 / 2)) * 1")
+
+        assertEquals("1 3 + 4 2 / - 1 *", result.trim())
+    }
+
+    @Test
+    fun testEval() {
+        val model = PostfixModelImpl()
+        val postfix = model.convertToPostfix("(1 + 3 - (4 / 2)) * 1")
+        val result = model.evaluate(postfix)
+
+        assertEquals(2, result)
+    }
+
+}
